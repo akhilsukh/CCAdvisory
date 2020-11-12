56efb0fcae2710db5aa91dcdeeff5d43137f0063
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-11-11 23:00:23 -0800

    Change About page titles

--- components/SubContainer.js
+++ components/SubContainer.js
@@ -4,7 +4,7 @@ function SubContainer (props){
     return (
         <div className="bg-teal-900 rounded-lg block my-8 p-4" id={props.id}>
             <h3 className="subcontainer-text">{props.title}</h3>
-            <div className="lg:px-4 xl:px-6 xl:pb-2">{props.children}</div>
+            <div className="lg:px-4 xl:px-6">{props.children}</div>
         </div>
     )
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -11,7 +11,7 @@ function ProfileCard(props) {
         <div>
           <img className="rounded-full mx-auto bg-green-200 h-32 w-32 m-2 border border-r-1" src={image}></img>
           <h6 className="text-white text-lg font-medium mt-1 truncate w-full">{name}</h6>
-          <p className="text-gray-100 text-xs font-medium w-full inline">{role}</p>
+          <p className="text-gray-100 text-xs w-full inline">{role}</p>
           <p className="text-xs text-gray-400 w-full block">{classes}</p>
         </div>
       </Link>
@@ -25,17 +25,17 @@ function About() {
       <div className="flex flex-col lg:w-3/5 xl:w-3/5">
         <SubContainer title="Founders">
           <ul className="grid justify-items-center gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-            <ProfileCard name="Akhil Sukhthankar" classes="Computer Science" role="Director, Developer" image={require('../../public/images/akh.jpg')}/>
-            <ProfileCard name="Barr Avrahamov" classes="Computer Science" role="Director of Outreach" image={require('../../public/images/bar.jpg')}/>
-            <ProfileCard name="Chris Hoeft" classes="Political Science" role="Director of Content" image={require('../../public/images/chr.jpg')}/>
+            <ProfileCard name="Akhil Sukhthankar" classes="De Anza - Computer Science" role="Director, Developer" image={require('../../public/images/akh.jpg')}/>
+            <ProfileCard name="Barr Avrahamov" classes="De Anza - Computer Science" role="Director of Outreach" image={require('../../public/images/bar.jpg')}/>
+            <ProfileCard name="Chris Hoeft" classes="De Anza - Political Science" role="Director of Content" image={require('../../public/images/chr.jpg')}/>
           </ul>
         </SubContainer>
 
         <SubContainer title="Writers">
           <ul className="grid justify-items-center gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-            <ProfileCard name="Hassham Malik" classes="Computer Science" image={require('../../public/images/has.jpg')}/>
-            <ProfileCard name="Preethan Selva" classes="Business" image={require('../../public/images/pre.jpg')}/>
-            <ProfileCard name="Matthew Matulewicz" classes="Communications" image={require('../../public/images/mat.jpg')}/>
+            <ProfileCard name="Hassham Malik" classes="De Anza - Computer Science" image={require('../../public/images/has.jpg')}/>
+            <ProfileCard name="Preethan Selva" classes="De Anza - Business" image={require('../../public/images/pre.jpg')}/>
+            <ProfileCard name="Matthew Matulewicz" classes="De Anza - Communications" image={require('../../public/images/mat.jpg')}/>
           </ul>
         </SubContainer>
 
