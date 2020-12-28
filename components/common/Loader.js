b697cf11e7b26b46b3fcc7c1e1e2de516f5f0ec3
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-12-28 14:14:58 -0800

    Reduce y-margin size

--- components/common/Loader.js
+++ components/common/Loader.js
@@ -2,7 +2,7 @@ import HashLoader from "react-spinners/HashLoader";
 
 function Loader(props) {
   return (
-    <div className="my-12 mx-auto flex justify-center">
+    <div className="my-6 mx-auto flex justify-center">
       <HashLoader loading={props.loading} color={"#0C4A6E"}></HashLoader>
     </div>
   )
