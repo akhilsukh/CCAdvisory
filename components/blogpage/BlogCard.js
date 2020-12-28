5418391da39fbc35828f250e19bf649bdbda82ec
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-12-28 09:22:58 -0800

    Change design to all-gray

--- components/blogpage/BlogCard.js
+++ components/blogpage/BlogCard.js
@@ -24,7 +24,7 @@ function BlogCard(props) {
     <Link href={`/blog/${post}`}>
       <div className="flex cursor-pointer w-full ring-1 ring-gray-200 rounded-lg overflow-hidden blog-card hover:shadow">
         {/* <div className={band_color}/> */}
-        <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer flex-col p-3 w-full blog-card-content">
+        <div className="bg-gray-150 hover:bg-gray-200 cursor-pointer flex-col p-3 w-full blog-card-content">
           <p className="text-yellow-600 uppercase text-xs font-medium truncate w-full">{section}</p>
           <p className="font-medium text-lg truncate w-full">{title}</p>
           <div className="flex justify-between">
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
           200: "#B6DBF0",
           800: '#0C4A6E',
           900: '#063C5B'
+        },
+        gray: {
+          150: "#ECEEF1"
         }
       }
     }
